be3242a255bedf965b59f3288c42f1a580e77264
Lucas Sandim <lucasandim_gd1@hotmail.com>
2020-10-06 19:40:19 -0400

    adicionado filtro para busca de digimons; adicionado tambem um icone de loading

--- src/components/ListaDigimon.vue
+++ src/components/ListaDigimon.vue
@@ -1,8 +1,17 @@
 <template>
   <v-container>
-        <v-card dark v-for="digimon in digimons" v-bind:key="digimon" class="ma-5"
-         v-bind:style="{ backgroundColor: selecionaCor(digimon.level)}"
-        >
+        <div>
+            <v-text-field type="text" class="ma-15" label="Buscar Digimon por nome" v-model="busca" /> 
+        </div>
+        <v-row v-show="carregando" style="justify-content: center" class="ma-15">
+            <breeding-rhombus-spinner
+            :animation-duration="1000"
+            :size="80"
+            color="#3C096C"/>
+        </v-row>
+ 
+        <v-card dark v-for="digimon in DigimonsFiltrados" v-bind:key="digimon" class="ma-5"
+         v-bind:style="{ backgroundColor: selecionaCor(digimon.level)}">
             <div class="d-flex flex-no-wrap justify-space-between" >
                 <div>
                     <v-card-title class="headline" v-text="digimon.name">
@@ -19,12 +28,17 @@
 
 <script>
 import axios from 'axios'
+import {BreedingRhombusSpinner} from 'epic-spinners'
+
   export default {
     name: 'ListaDigimon',
-
+    components: {
+      BreedingRhombusSpinner
+    },
     data: () => ({
+        busca: '',
+        carregando: false,
         digimons: [],
-        teste: 'tesssttee',
         colorByLevels: {'In Training': '#E0AAFF', 'Training': '#C77DFF', 'Rookie': '#9D4EDD', 'Champion': '#7B2CBF', 'Ultimate': '#5A189A', 'Fresh': '#3C096C', 'Mega': '#240046', 'Armor': '#10002B'}
     }),
     mounted () {
@@ -32,17 +46,29 @@ import axios from 'axios'
     },
     methods:{
         requisicaoDigimons: function(){
+            this.carregando = true
             axios
             .get('https://digimon-api.herokuapp.com/api/digimon')
-            .then(response => (
+            .then(response => {
                 this.digimons = response.data
-            ))
+                this.carregando = false
+            })
             .catch(e => {
                 console.error("Erro ao realizar requisição", e)
+                this.carregando = false
             })
         },
         selecionaCor: function(level){
             return this.colorByLevels[level]
+        }
+    },
+    computed: {
+        DigimonsFiltrados() {
+            this.carregando = true
+            return this.digimons.filter(digimon => {
+                this.carregando = false
+                return digimon.name.toLowerCase().includes(this.busca.toLowerCase())
+            })
         }
     }
   }
